400fe6750b81032fd43597dc91cbb7cef8fcd61b
erick91 <erickmorales91@outlook.com>
2020-05-15 03:05:46 -0400

    se genera metodo de exportar a excel desde reporte stock

--- Portafolio-Escritorio/Views/UserControlReporteStock.xaml.cs
+++ Portafolio-Escritorio/Views/UserControlReporteStock.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OracleClient;
+using System.Data;
 
 namespace Portafolio_Escritorio.Views
 {
@@ -20,9 +24,40 @@ namespace Portafolio_Escritorio.Views
     /// </summary>
     public partial class UserControlReporteStock : UserControl
     {
+        OracleConnection conexion = new OracleConnection("DATA SOURCE = xe; PASSWORD= 1234; USER ID= ERICK;");
         public UserControlReporteStock()
         {
             InitializeComponent();
+        }
+
+        private void btn_stock_excel_Click(object sender, RoutedEventArgs e)
+        {
+            dgv_reporte_stock.SelectAllCells();
+            dgv_reporte_stock.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+            ApplicationCommands.Copy.Execute(null, dgv_reporte_stock);
+            String resultat = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+            String result = (string)Clipboard.GetData(DataFormats.Text);
+            dgv_reporte_stock.UnselectAllCells();
+            System.IO.StreamWriter file1 = new System.IO.StreamWriter(@"C:\Users\Emy&Tamy\Desktop\Stock.xls");
+            file1.WriteLine(result.Replace(',', ' '));
+            file1.Close();
+
+            MessageBox.Show(" Se ha generado el archivo excel Stock.xls con exito");
+        }
+
+        private void dgv_reporte_stock_Loaded(object sender, RoutedEventArgs e)
+        {
+            conexion.Open();
+            OracleCommand comando = new OracleCommand("seleccionarProductos", conexion);
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.Add("registros", OracleType.Cursor).Direction = ParameterDirection.Output;
+
+            OracleDataAdapter adaptador = new OracleDataAdapter();
+            adaptador.SelectCommand = comando;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dgv_reporte_stock.ItemsSource = tabla.DefaultView;
+            conexion.Close();
         }
     }
 }
